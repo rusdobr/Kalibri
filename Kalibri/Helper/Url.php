9eac3bb9feadfb6eacff7be4a0181ff27c2a420c
tenebras <kostinenko@gmail.com>
2013-11-06 22:38:48 +0200

    fix site url (pasible // in url)

--- Kalibri/Helper/Url.php
+++ Kalibri/Helper/Url.php
@@ -97,7 +97,9 @@ namespace Kalibri\Helper {
 				? self::$baseUrl
 				: str_replace('%d%', $subdomain, self::$baseDomainedUrl);
 			
-			return $root.( $path && $path[0] !== '/' && $root[ strlen( $root )-1 ] != '/' ? '/':''). $path;
+			$path = $path && $path[0] == '/'? substr($path, 1): $path;
+			
+			return $root.($root[strlen($root)-1] != '/'? '/': '').$path;
 		}
 
 //------------------------------------------------------------------------------------------------//
